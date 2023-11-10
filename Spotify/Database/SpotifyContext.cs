6d7d3a45b690910858a1de99ffd5c06481c1a35b
rafae <rafae@PAULA_MARQUES>
2023-11-10 11:48:57 +0000

    Adicionar arquivos de projeto.

---
+++ Spotify/Database/SpotifyContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Spotify.Database {
+    public class SpotifyContext :DbContext
+    {
+
+        public SpotifyContext() {
+            this.Database.Connection.ConnectionString = "Server=SINF113\\SQLEXPRESS; Database=Projeto-Spotify; User Id=sa; Password=SQLEXPRESS_;";
+        } 
+    }
+}
