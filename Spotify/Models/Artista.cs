3092277af650c6c9f3a85ea20f01860b9b2ae783
R4F4EL <rafaelbru2006@gmail.com>
2023-11-14 13:14:18 +0000

    Models e BD

---
+++ Spotify/Models/Artista.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Spotify.Models
+{
+    public class Artista
+    {
+        [Key]
+        public int IDArtista { get; set; }
+
+        [Required]
+        public string NomeArtista { get; set; }
+
+        [Required]
+        public string ImagemPerfil { get; set; }
+
+        [Required]
+        public DateTime DataNasc { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public bool Verificado { get; set; }
+
+        [Required]
+        public int Streams { get; set; }
+
+        //[Key]
+        //[ForeignKey("IDAlbum")]
+        //public ICollection<Album> Albuns { get; set; }
+
+        //[Key]
+        //[ForeignKey("IDArtista")]
+        //public ICollection<Musica> Musicas { get; set; }
+    }
+}
