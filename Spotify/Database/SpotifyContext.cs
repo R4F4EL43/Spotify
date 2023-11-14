af87a135f4fa4f1160dd60993521290488580f35
R4F4EL43 <113055661+R4F4EL43@users.noreply.github.com>
2023-11-14 12:54:02 +0000

    Add files via upload

--- Spotify/Database/SpotifyContext.cs
+++ Spotify/Database/SpotifyContext.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Spotify.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace Spotify.Database {
-    public class SpotifyContext :DbContext
+namespace Spotify.Database
+{
+    public class SpotifyContext : DbContext
     {
+        public DbSet<Musica> Musica { get; set; }
+        public DbSet<Album> Albuns { get; set; }
+        public DbSet<Artista> Artistas { get; set; }
 
-        public SpotifyContext() {
+        public SpotifyContext()
+        {
             this.Database.Connection.ConnectionString = "Server=SINF113\\SQLEXPRESS; Database=Projeto-Spotify; User Id=sa; Password=SQLEXPRESS_;";
-        } 
+        }
+
+
     }
 }
